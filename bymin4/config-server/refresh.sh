27dee98e02d3c498108098fbcd1a505ad1cc950a
bymin4 <bymin4@coupang.com>
2021-10-25 21:01:33 +0900

    add

--- bymin4/config-server/refresh.sh
+++ bymin4/config-server/refresh.sh
@@ -5,16 +5,3 @@ docker rmi config-server
 docker build ./ -t config-server:latest
 docker run --name config-server --network edu -p 8888:8888 config-server:latest
 
-
-
-
-cd ./bymin4/config-server/
-./gradlew bootJar
-docker build ./ -t config-server:latest
-
-
-
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-wget "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
